ee479457571d61f5aac07250984a25f903305f4c
Daniel Menchaca Luna <dannywolfmx@gmail.com>
2020-03-16 16:59:00 -0600

    Mostrar iconos en windows
    Ahora la aplicacion muestra los iconos cuando sube, baja o se mantiene el tipo de cambio

--- notificacion/notificacion_windows.go
+++ notificacion/notificacion_windows.go
@@ -2,6 +2,8 @@
 package notificacion
 
 import (
+	"path/filepath"
+
 	"gopkg.in/toast.v1"
 )
 
@@ -20,17 +22,26 @@ func (n *notificacion) FijaMensaje(mensaje string) {
 }
 
 func (n *notificacion) Mostrar(tipoAlerta float32) error {
+	icon := ""
+	var err error
 	if tipoAlerta > 0 {
 		n.toast.Title = "Subio el tipo de cambio"
+		icon, err = filepath.Abs("up-red.png")
 	} else if tipoAlerta == 0 {
 		n.toast.Title = "Tipo de cambio igual"
+		icon, err = filepath.Abs("equal.png")
 	} else {
 		n.toast.Title = "Bajo el tipo de cambio"
+		icon, err = filepath.Abs("down-green.png")
+	}
+	if err == nil {
+		n.toast.Icon = icon
 	}
 	return n.toast.Push()
 }
 
 func crearToast() *toast.Notification {
+
 	notification := &toast.Notification{
 		AppID: "Tipo de cambio",
 	}
